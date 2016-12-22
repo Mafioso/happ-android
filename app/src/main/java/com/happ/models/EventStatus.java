fd38e954c2e1024f556a22c03a8c8221cb696dff
Timur Izmagambetov <iztiev@gmail.com>
2016-12-22 19:46:18 +0600

    filter

---
+++ app/src/main/java/com/happ/models/EventStatus.java
@@ -0,0 +1,12 @@
+package com.happ.models;
+
+/**
+ * Created by iztiev on 12/22/16.
+ */
+
+public class EventStatus {
+    public static final int ON_REVIEW = 0;
+    public static final int ACTIVE = 1;
+    public static final int REJECTED = 2;
+    public static final int FINISHED = 10000;
+}
